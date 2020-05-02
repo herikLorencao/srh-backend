cf1b58ede7583c0b95e86c2b047ac75dfdd6e2fb
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2020-05-02 16:10:17 -0300

    feat: adicionando estrutura inicial
    Adicionando scripts de automatização do git e estrutura inicial do spring

---
+++ src/main/java/com/srh/api/HybridRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.srh.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridRecommendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HybridRecommendationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srh/api/HybridRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srh.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridRecommendationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
