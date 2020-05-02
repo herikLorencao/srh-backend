02f5ebcd285b7f5b619bf8e1f9721078418555fb
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2020-05-02 16:31:46 -0300

    feat: adicionando application.properties e dependência de jwt
    Adicionando configurações do arquivo application.properties e dependência do jwt para validação de
    token

--- src/main/java/com/srh/api/Application.java
+++ src/main/java/com/srh/api/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HybridRecommendationSystemApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HybridRecommendationSystemApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
